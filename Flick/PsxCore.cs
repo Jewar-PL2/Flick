3ecae511c3a6da8167f64938dd2cafba79c07894
Jewar-PL <a.batorski.jewar@gmail.com>
2025-01-22 21:53:51 +0100

    Add address masking, panic on unhandled access

--- Flick/PsxCore.cs
+++ Flick/PsxCore.cs
@@ -5,6 +5,16 @@ public class PsxCore
     // Todo: Perhaps replace array with pointer if it boosts performance
     private byte[] bios;
 
+    private static uint[] RegionMasks =
+    [
+        0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF, // KUSEG: 2048MB
+        0x7FFFFFFF,                                     // KSEG0:  512MB
+        0x1FFFFFFF,                                     // KSEG1:  512MB
+        0xFFFFFFFF, 0xFFFFFFFF,                         // KSEG2: 1024MB
+    ];
+
+    public static uint GetMaskedAddress(uint address) => address & RegionMasks[address >> 29];
+
     public PsxCore(string biosPath)
     {
         // Todo: Verify if BIOS size is correct
@@ -13,9 +23,11 @@ public class PsxCore
 
     public uint Read32(uint address)
     {
-        if (address >= 0xBFC00000 && address < 0xBFC80000)
+        address = GetMaskedAddress(address);
+        
+        if (address >= 0x1FC00000 && address < 0x1FC80000)
         {
-            return BitConverter.ToUInt32(bios, (int)(address - 0xBFC00000));
+            return BitConverter.ToUInt32(bios, (int)(address - 0x1FC00000));
         }
 
         return 0x00;
@@ -23,6 +35,23 @@ public class PsxCore
 
     public void Write32(uint address, uint value)
     {
-        Utility.Log($"PSXCORE: Unhandled Write32 to 0x{address:X8}: 0x{value:X8}");
+        address = GetMaskedAddress(address);
+
+        if (address >= 0x1F801000 && address < 0x1F801040)
+        {
+            Utility.Log($"PSXCORE: Unhandled Write32 to MEMORY CONTROL 1 at 0x{address:X8}: 0x{value:X8}");
+        }
+        else if (address >= 0x1F801060 && address < 0x1F801064)
+        {
+            Utility.Log($"PSXCORE: Unhandled Write32 to RAM_SIZE at 0x{address:X8}: 0x{value:X8}");
+        }
+        else if (address == 0xFFFE0130)
+        {
+            Utility.Log($"PSXCORE: Unhandled Write32 to CACHE CONTROL at 0x{address:X8}: 0x{value:X8}");
+        }
+        else
+        {
+            Utility.Panic($"PSXCORE: Unhandled Write32 to 0x{address:X8}: 0x{value:X8}");
+        }
     }
 }
