04045336fe01c87feb58d52b768f3b044f145399
Jewar-PL <a.batorski.jewar@gmail.com>
2025-01-22 19:58:36 +0100

    Add Execute method and Instruction as a CPU field

--- Flick/Processor/R3000.cs
+++ Flick/Processor/R3000.cs
@@ -9,12 +9,17 @@ public class R3000
     private uint programCounter;
     private uint nextProgramCounter;
 
+    private Instruction instruction;
+
     public R3000(PsxCore core)
     {
         this.core = core;
+        
         registers = new uint[32];
         programCounter = 0xBFC00000;
         nextProgramCounter = programCounter + 4;
+        
+        instruction = new Instruction(0);
     }
 
     private Instruction FetchInstruction()
@@ -25,11 +30,21 @@ public class R3000
 
     public void Step()
     {
-        Instruction instruction = FetchInstruction();
+        instruction = FetchInstruction();
 
         programCounter = nextProgramCounter;
         nextProgramCounter = programCounter + 4;
         
         // what now?
+    }
+
+    private void Execute()
+    {
+        switch (instruction.Opcode)
+        {
+            default:
+                Utility.Panic($"Unhandled instruction: {instruction.Raw:X8}");
+                break;
+        }
     }
 }
