ae183dc2752478f48c8862f16c8d08ac1f8de29d
Jewar-PL <a.batorski.jewar@gmail.com>
2025-01-22 22:49:38 +0100

    Add RAM to PsxCore

--- Flick/PsxCore.cs
+++ Flick/PsxCore.cs
@@ -4,6 +4,7 @@ public class PsxCore
 {
     // Todo: Perhaps replace array with pointer if it boosts performance
     private byte[] bios;
+    private byte[] ram;
 
     private static uint[] RegionMasks =
     [
@@ -19,18 +20,27 @@ public class PsxCore
     {
         // Todo: Verify if BIOS size is correct
         bios = File.ReadAllBytes(biosPath);
+        ram = new byte[2 * 1024 * 1024];
     }
 
     public uint Read32(uint address)
     {
         address = GetMaskedAddress(address);
-        
-        if (address >= 0x1FC00000 && address < 0x1FC80000)
+
+        if (address < 0x1F000000)
+        {
+            // 2 MB RAM is mirrored
+            return BitConverter.ToUInt32(ram, (int)(address & 0x1FFFFF));
+        }
+        else if (address >= 0x1FC00000 && address < 0x1FC80000)
         {
             return BitConverter.ToUInt32(bios, (int)(address - 0x1FC00000));
         }
-
-        return 0x00;
+        else
+        {
+            Utility.Panic($"PSXCORE: Unhandled Read32 from 0x{address:X8}");
+            return 0x00;
+        }
     }
 
     public void Write32(uint address, uint value)
