61309c9c03bfe2902ff25e0721c8c421db7b639d
Jewar-PL <a.batorski.jewar@gmail.com>
2025-01-22 19:55:08 +0100

    Add Utility class

---
+++ Flick/Utility.cs
@@ -0,0 +1,14 @@
+﻿namespace Flick;
+
+public static class Utility
+{
+    public static void Panic(string message)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine(message);
+        Console.ResetColor();
+
+        // Todo: Replace this with a better Exception
+        throw new Exception("Panic");
+    }
+}
