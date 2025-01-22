f26aefedae05231e9e3edc4da0ffac8663c7723b
Jewar-PL <a.batorski.jewar@gmail.com>
2025-01-22 22:55:55 +0100

    Add util functions to modify data in byte arrays

--- Flick/Utility.cs
+++ Flick/Utility.cs
@@ -18,4 +18,28 @@ public static class Utility
         Console.WriteLine("[LOG] " + message);
         Console.ResetColor();
     }
+
+    public static uint ReadUInt32(byte[] buffer, uint offset)
+    {
+        return BitConverter.ToUInt32(buffer, (int)offset);
+    }
+    
+    public static ushort ReadUInt16(byte[] buffer, uint offset)
+    {
+        return BitConverter.ToUInt16(buffer, (int)offset);
+    }
+    
+    public static void WriteUInt32(byte[] buffer, uint offset, uint value)
+    {
+        buffer[offset + 0] = (byte)(value >> 0);
+        buffer[offset + 1] = (byte)(value >> 8);
+        buffer[offset + 2] = (byte)(value >> 16);
+        buffer[offset + 3] = (byte)(value >> 24);
+    }
+    
+    public static void WriteUInt16(byte[] buffer, uint offset, ushort value)
+    {
+        buffer[offset + 0] = (byte)(value >> 0);
+        buffer[offset + 1] = (byte)(value >> 8);
+    }
 }
